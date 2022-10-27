4281652cebbefb541742d609c67a9112092d4729
Stepan <stepakiselev@mail.ru>
2022-10-27 15:43:45 +0600

    Create fill_orbit_with_garbage.py

---
+++ fill_orbit_with_garbage.py
@@ -0,0 +1,12 @@
+from main import wait
+
+
+async def fill_orbit_with_garbage(canvas, y_axis, x_axis, element, speed):
+  while True:
+    canvas.addstr(y_axis, x_axis, element, speed)
+    await wait(2)
+
+    y_axis += speed
+    canvas.addstr(y_axis, x_axis, element, speed)
+    await wait(2)
+    
